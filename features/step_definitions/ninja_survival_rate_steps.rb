cac6606ab0c470ae70bb17ab017a7c2a99c5785d
Simon Fenton <swfenton@gmail.com>
2011-04-10 17:04:50 +1000

    Initial commit, working through the cuke4ninja book

---
+++ features/step_definitions/ninja_survival_rate_steps.rb
@@ -0,0 +1,20 @@
+  Given /^the ninja has a ([a-z]*) level black\-belt$/ do |belt_level|
+    @ninja=Ninja.new belt_level
+  end
+  
+  When "attacked by a samurai" do
+    pending
+  end
+  
+  When "attacked by Chuck Norris" do
+    pending
+  end
+  
+  Then "the ninja should engage the opponent" do
+    pending
+  end
+  
+  Then "the ninja should run for his life" do
+    pending
+  end
+  
